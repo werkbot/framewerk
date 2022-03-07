5d040778d87ac5892764717a6fbd20140d862640
Tyler <tiller1010@gmail.com>
2022-03-07 11:20:37 -0500

    Post css loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,16 @@ module.exports = {
 						},
 					},
 					{
+						loader: "postcss-loader",
+						options: {
+							postcssOptions: {
+								plugins: [
+									'autoprefixer',
+								],
+							},
+						},
+					},
+					{
 						loader: 'sass-loader',
 					}
 				]
