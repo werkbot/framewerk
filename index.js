aa158a4d737f8d2fe1651b77e5bf690d988be98c
Tyler <tiller1010@gmail.com>
2022-03-18 17:06:15 -0400

    Module exports

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-module.exports = require('./dist/framewerk.js')
+function setCSSVar(element, name, value){
+	element.style.setProperty('--' + name, value);
+}
 
-module.exports.initFramewerk = function(config = null){
-	var root = document.querySelector(':root');
-	function setCSSVar(name, value){
-		root.style.setProperty('--' + name, value);
+const customizeFramewerk = (config = null, elementSelector = null) => {
+	let element;
+	if(elementSelector){
+		element = document.querySelector(elementSelector);
+	} else {
+		element = document.querySelector(':root');
 	}
 	if(config){
 		for(var prop in config){
-			setCSSVar(prop, config[prop]);
+			setCSSVar(element, prop, config[prop]);
 		}
 	}
 }
+
+
+/*
+	Import with one of the following
+	const framewerk = require('framewerk');
+	import framewerk from 'framewerk'
+*/
+module.exports = (config = null, element = null) => {
+	require('./dist/framewerk.js')
+	customizeFramewerk(config, element);
+}
+
+/*
+	Import with one of the following
+	const { initFramewerk } = require('framewerk');
+	import { initFramewerk } from 'framewerk';
+*/
+module.exports.initFramewerk = () => require('./dist/framewerk.js');
+
+
+/*
+	Import with one of the following
+	const { customizeFramewerk } = require('framewerk');
+	import { customizeFramewerk } from 'framewerk';
+*/
+module.exports.customizeFramewerk = customizeFramewerk;
