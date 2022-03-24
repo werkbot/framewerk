882f553dbb96a6219d191f8286d53c03825a0048
Tyler <tiller1010@gmail.com>
2022-03-24 11:47:12 -0400

    Recursively set component properties, button variants

--- index.js
+++ index.js
@@ -2,6 +2,51 @@ function setCSSVar(element, name, value){
 	element.style.setProperty('--' + name, value);
 }
 
+function injectButtonStyles(variants){
+	var injectedStylesheet = (function() {
+		var style = document.createElement('style');
+		style.appendChild(document.createTextNode(''));
+		document.head.appendChild(style);
+		return style.sheet;
+	})();
+
+	for(var btnClass in variants){
+		var btnStyleString = `.${btnClass}{`;
+		var btnHoverStyleString = `.${btnClass}:hover{`;
+		var btnStyleRules = variants[btnClass];
+		for(var btnStyleRule in btnStyleRules){
+			var btnStyleValue = btnStyleRules[btnStyleRule];
+			if(btnStyleRule == 'hoverStyle'){
+				var btnHoverStyleRules = btnStyleRules.hoverStyle;
+				for(var btnHoverStyleRule in btnHoverStyleRules){
+					var btnHoverStyleValue = btnHoverStyleRules[btnHoverStyleRule];
+					btnHoverStyleString += `${btnHoverStyleRule}: var(--componentButtonProperties_variants_${btnClass}_hoverStyle_${btnHoverStyleRule}, ${btnHoverStyleValue}) !important;`
+				}
+			} else {
+				btnStyleString += `${btnStyleRule}: var(--componentButtonProperties_variants_${btnClass}_${btnStyleRule}, ${btnStyleValue}) !important;`
+			}
+		}
+		btnStyleString += '}';
+		btnHoverStyleString += '}';
+		injectedStylesheet.insertRule(btnStyleString, 0);
+		injectedStylesheet.insertRule(btnHoverStyleString, 0);
+	}
+}
+
+function recursivelySetCSSVars(element, config, scopePrefix = ''){
+	for(var prop in config){
+		if(typeof config[prop] == 'object'){
+			var componentConfig = config[prop];
+			if(prop == 'variants'){
+				injectButtonStyles(config.variants);
+			}
+			recursivelySetCSSVars(element, componentConfig, scopePrefix ? scopePrefix + '_' + prop : prop);
+		} else {
+			setCSSVar(element, scopePrefix ? scopePrefix + '_' + prop : prop, config[prop]);
+		}
+	}
+}
+
 const customizeFramewerk = (config = null, elementSelector = null) => {
 	let element;
 	if(elementSelector){
@@ -10,16 +55,7 @@ const customizeFramewerk = (config = null, elementSelector = null) => {
 		element = document.querySelector(':root');
 	}
 	if(config){
-		for(var prop in config){
-			if(typeof config[prop] == 'object'){
-				var componentConfig = config[prop];
-				for(var componentProp in componentConfig){
-					setCSSVar(element, prop + '_' + componentProp, componentConfig[componentProp]);
-				}
-			} else {
-				setCSSVar(element, prop, config[prop]);
-			}
-		}
+		recursivelySetCSSVars(element, config);
 	}
 }
 
