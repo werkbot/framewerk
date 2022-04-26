a91ee446b72fc5bb3e1b5c9c8709486e5f9eccce
Jay Richardson <jay@werkbot.com>
2022-04-26 14:30:45 -0400

    Updated form javascript
    - Fields are now set to readonly instead of being disabled
    - Updated the overall style/look, this simply adds a css animated option next to the submit button
    - Added a listener for a reset event on the form, useful for ajax calls. This requires the reset function to be called though

--- js/form.js
+++ js/form.js
@@ -29,26 +29,61 @@ window.addEventListener('load', function(){
    */
   document.querySelectorAll('.fw-form').forEach(function(formElement){
     formElement.addEventListener('submit', function(event){
-      // Disable submit button, hide the button
-      event.submitter.disabled = true;
-      event.submitter.style.display = "none";
+      // Check the validity of the form, if any fields are not valid do not move forward
+      if(!this.checkValidity()){
+        // Invalid form elements, halt
+        e.preventDefault();
+      }else{
+        // create wrapper container if it does not exist already
+        var wrapper = this.querySelector('.fw-form-processing-container');
+        if(!wrapper){
+          wrapper = document.createElement('div');
+          wrapper.className = "fw-form-processing-container";
+          // insert wrapper before el in the DOM tree
+          event.submitter.parentNode.insertBefore(wrapper, event.submitter);
+          // move el into wrapper
+          wrapper.appendChild(event.submitter); 
+        }
 
-      // Add "Processing" element to replace submit button
-      const processingElement = document.createElement("span");
-      processingElement.className = "fw-form-processing";
-      this.appendChild(processingElement);
+        // Create "Processing" element 
+        const processingElement = document.createElement("span");
+        processingElement.className = "fw-form-processing";
+        wrapper.appendChild(processingElement); 
 
-      // Disable all fields
+        // Disable submit button
+        event.submitter.disabled = true;
+
+        // Disable all fields
+        var fields = this.getElementsByTagName('*');
+        for(var i = 0; i < fields.length; i++)
+        {
+          fields[i].classList.add("readonly");
+          // Check for number input type
+          if(fields[i].type=="number"){
+            fields[i].parentElement.parentElement.classList.add("readonly");
+          }
+        }
+      }
+    });
+    
+    // Listener for a form reset event, useful for ajax calls.
+    // This will reset the form to its original state
+    formElement.addEventListener('reset', function(event){
+      // Remove the processing element
+      const p = this.querySelector('.fw-form-processing');
+      p.parentElement.removeChild(p);
+
+      // Re-enable all fields
       var fields = this.getElementsByTagName('*');
       for(var i = 0; i < fields.length; i++)
       {
-        fields[i].disabled = true;
+        fields[i].classList.remove("readonly");
+        fields[i].disabled = false;
         // Check for number input type
         if(fields[i].type=="number"){
-          fields[i].parentElement.parentElement.classList.add("readonly");
+          fields[i].parentElement.parentElement.classList.remove("readonly");
         }
       }
-
     });
   });
 
